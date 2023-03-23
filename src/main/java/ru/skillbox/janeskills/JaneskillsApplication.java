cba4aabee82b574725491c5d23b58e0797a27b33
Maksim Bulenkov <m.bulenkov@vk.team>
2023-03-23 23:29:00 +0300

    init commit

---
+++ src/main/java/ru/skillbox/janeskills/JaneskillsApplication.java
@@ -0,0 +1,13 @@
+package ru.skillbox.janeskills;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaneskillsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaneskillsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/skillbox/janeskills/JaneskillsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skillbox.janeskills;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaneskillsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
